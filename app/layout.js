29e5461065bf16f6f3cd37120af48e7776aa13b3
pintubeservi-star <pintubeservi@gmail.com>
2026-04-12 14:52:16 -0400

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,3 @@
-/**
- * Ubicación: /app/layout.js
- */
 export const metadata = {
   title: "Kolma POS | Administrador",
   description: "Sistema de ventas para Kolma RD",
@@ -9,9 +6,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
-      <body style={{ margin: 0, padding: 0, backgroundColor: '#f1f5f9' }}>
+      <body style={{ margin: 0, padding: 0 }}>
         {children}
       </body>
     </html>
   );
-}
+        }
