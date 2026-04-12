32d5bb1468711ad3ab2446adc287bd68033864b5
pintubeservi-star <pintubeservi@gmail.com>
2026-04-12 15:05:49 -0400

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,11 @@
 import "./globals.css";
+
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
-      <body className="antialiased bg-slate-50">{children}</body>
+      <body className="antialiased bg-gray-50 text-slate-900">
+        {children}
+      </body>
     </html>
   );
 }
