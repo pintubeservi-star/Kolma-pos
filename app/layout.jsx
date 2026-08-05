99c85bd5febff2ca2c094949d8d4ff08f2b31486
pintubeservi-star <pintubeservi@gmail.com>
2026-08-05 12:29:22 -0400

    Delete app/layout.jsx

--- app/layout.jsx
+++
@@ -1,9 +0,0 @@
-import './globals.css';
-
-export default function RootLayout({ children }) {
-  return (
-    <html lang="es">
-      <body className="bg-black text-white">{children}</body>
-    </html>
-  );
-}
