a7d9b3ff13f896d5fa24bd52d68f15513ee6eb23
pintubeservi-star <pintubeservi@gmail.com>
2026-09-11 11:47:14 -0400

    Eliminar layout.js

--- app/layout.js
+++
@@ -1,9 +0,0 @@
-import './globals.css';
-
-export default function RootLayout({ children }) {
-  return (
-    <html lang="es">
-      <body className="bg-black text-white min-h-screen">{children}</body>
-    </html>
-  );
-}
